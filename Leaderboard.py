0b9c24209d79646092bc89c911643031b98cb77e
Benjy Hollister <benjyhollister@gmail.com>
2025-10-12 00:04:43 +0100

    Skeleton of the program: with buttons and frames up to date

--- Leaderboard.py
+++ Leaderboard.py
@@ -12,29 +12,75 @@ window.columnconfigure(0, weight=1)
 menu_frame = tk.Frame(window)
 menu_frame.grid(row=0, column=0, sticky="nsew")
 
+lb_frame = tk.Frame(window)
+lb_frame.grid(row=0, column=0, sticky="nsew")
+
+
+
+#Button functions
+
+def exit():
+   quit() #closes the tkinter window
+
+def help():
+   help_frame = tk.Frame(menu_frame, bg="lightgrey")
+   help_frame.place(relx=0.5, rely=0.5, anchor="center")
+
+   helpLabel = tk.Label(help_frame, text="This explains everything", bg="lightgrey")
+   helpLabel.pack(padx=10, pady=10)
+   helpButton = tk.Button(help_frame, text="Close", command=help_frame.destroy)
+   helpButton.pack(pady=5)
+   
+def toLeaderboard():
+   lb_frame.tkraise()
+
+
+#def add():
+   #this should be a dropdown within the leaderboard, and able to sdelect new score or new user
+
+#def delete():
+
+def back():
+   menu_frame.tkraise()
+
+
+
+#Main menu frame
 label = tk.Label(menu_frame, text="Welcome to the Leaderboards", font=('Arial', 18))
 label.pack(padx=20, pady=20)
 
 label = tk.Label(menu_frame, text="Main Menu", font=('Arial', 14))
 label.pack(padx=20, pady=30)
 
-
-btn1 = tk.Button(menu_frame, text="Leaderboard", width=25)
+btn1 = tk.Button(menu_frame, text="Leaderboard", width=25, command=toLeaderboard)
 btn1.pack(pady=10)
 
-btn2 = tk.Button(menu_frame, text="Help", width=25)
+btn2 = tk.Button(menu_frame, text="Help", width=25, command=help)
 btn2.pack(pady=10)
 
-btn3 = tk.Button(menu_frame, text="Exit", width=25)
+btn3 = tk.Button(menu_frame, text="Exit", width=25, command=exit)
 btn3.pack(pady=10)
+
+
+#Leaderboard frame
+label = tk.Label(lb_frame, text="Shot Speed", font=('Arial', 18))
+label.pack(padx=20, pady=20)
+
+lb_btn1 = tk.Button(lb_frame, text= "Add", width=15) #, command=add
+lb_btn1.pack(side=tk.LEFT, padx=10, pady=10)
+
+lb_btn2 = tk.Button(lb_frame, text = "Delete", width=15) #, command=delete
+lb_btn2.pack(side=tk.LEFT, padx=10, pady=10)
+
+lb_btn3 = tk.Button(lb_frame, text="Back", width=15, command=back)
+lb_btn3.pack(side=tk.LEFT, padx=10, pady=10)
 
 menu_frame.tkraise()
 
 window.mainloop()
 
-FILENAME = "leaderboard.json"    
-
 #So that the scores which are altered are saved and we don't just have a hard-coded dictionary, we save it to a file
+FILENAME = "leaderboard.json"    
 
 try:
    with open("leaderboard.json", "r") as f:
@@ -47,7 +93,7 @@ except FileNotFoundError:
       json.dump(Leaderboard, f)
 
 exit = False
-
+"""
 while exit == False:                    #add a break between when the last thing was executed til when the next choice is printed
 
    print("What would you like to do?")
@@ -87,7 +133,7 @@ while exit == False:                    #add a break between when the last thing
    
    else:
       print("No option selected, select another option")
-
+"""
 
 with open(FILENAME, "w") as f:
    json.dump(Leaderboard, f)
