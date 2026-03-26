75b77663dbafce52354302c68851ab1ae6df33ea
Benjy <benjyhollister@gmail.com>
2026-03-26 16:38:35 +0000

    Remove tkinter leaderboard and remove all terminal program

--- Leaderboard.py
+++ Leaderboard.py
@@ -1,40 +1,11 @@
 import json
-import tkinter as tk
-from tkinter import messagebox
-from tkinter import filedialog, ttk
 from flask import Flask 
 
 app = Flask(__name__)
 
-window = tk.Tk()
-window.title("window")
-window.geometry("400x600")
-
-window.rowconfigure(0, weight=1)
-window.columnconfigure(0, weight=1)
-
-menu_frame = tk.Frame(window)
-menu_frame.grid(row=0, column=0, sticky="nsew")
-
-lb_frame = tk.Frame(window)
-lb_frame.grid(row=0, column=0, sticky="nsew")
-
-
 #Leaderboard frame
 
-   #treeview 
-columns = ("Rank", "Name", "Score")
-tree = ttk.Treeview(lb_frame, columns=columns, show="headings", height=10)
-
-for col in columns:
-   tree.heading(col, text=col)
-   tree.column(col, anchor="center", width=100)
-
-tree.grid(row=1, column=0, columnspan=3, sticky="nsew", padx=5, pady=5)
-
-lb_frame.grid_rowconfigure(1, weight=1)
-lb_frame.grid_columnconfigure((0,1,2), weight=1)
-
+   #previously had displaying of leaderboard, so need to be done in flask
 
 
 # FILE HANDLING
@@ -280,62 +251,4 @@ lb_btn3.grid(row=2, column=2, pady=10)
 
 menu_frame.tkraise()
 window.mainloop()
-
-
-
-#Following is the program for the terminal output
-"""
-
-try:
-   with open("leaderboard.json", "r") as f:
-      Leaderboard = json.load(f)
-except FileNotFoundError:
-   Leaderboard = {'Benjy' : 50, "Sharlie" : 6, "Eliott" : 55} #use a dictionary to store names and scores
-
-   #save immediately to create file
-   with open(FILENAME, "w") as f:
-      json.dump(Leaderboard, f)
-
-exit = False
-
-while exit == False:                    #add a break between when the last thing was executed til when the next choice is printed
-
-   print("What would you like to do?")
-   print("1. View Leaderboard")
-   print("2. Add a score")
-   print("3. Add a user")
-   print("4. Delete a score")
-   print("5. Exit")# make a 5th option, 'exit', that cancels the program when selected, and the program otherwise always returns to the menu
-   choice = int(input("  : "))
-
-   if choice == 1:
-      print("Leaderboard: ")
-      for k,v in sorted(Leaderboard.items(), key=lambda item: item[1], reverse = True):
-         print(k,v)
-
-   elif choice == 2:
-      name = str(input("Who is this a new score for? "))
-      new_score = int(input(f"Enter the new score for {name}: " ))
-      Leaderboard[name] = new_score
-
-   elif choice == 3:
-      incomer = str(input("Who is the new user? "))
-      Leaderboard.update({incomer: 0})
-
-
-   elif choice == 4:
-      name_del = str(input("Whose score would you like to delete? "))
-      if name_del in Leaderboard:
-         Leaderboard.pop(name_del)
-         print(f"{name_del}'s score has been deleted")
-      else:
-         print(f"{name_del} was not found in leaderboard")
-   
-   elif choice == 5:
-      print("Exiting program...")
-      exit = True
-   
-   else:
-      print("No option selected, select another option")
-"""
 
